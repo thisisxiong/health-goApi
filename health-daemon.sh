7b14990045a3224c90e38ab891e3ace49d0e93c5
xiongwenhui <xiongwh@mail.open.com.cn>
2022-10-25 15:19:14 +0800

    update shell-log

--- health-daemon.sh
+++ health-daemon.sh
@@ -3,7 +3,7 @@ port=${1:-8080}
 while true; do
     num=$(ps -axu | grep -w health | grep -v grep | wc -l)
     if [ "$num" -lt 1 ]; then
-        ./health -port="$port" > start.log 2>&1
+        ./health -port="$port" > log/app_$(date +"%Y%m%d").log 2>&1
     fi
     sleep 10
 done
